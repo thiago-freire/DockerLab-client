b1679357d92977442384dfdfbee66e474d7c79fa
Thiago Paiva Freire <thiago.freire@ifma.edu.br>
2025-03-12 00:08:43 -0300

    [FIX] Ajustes para 'run build'

--- src/app/components/Forms/Auth/NCAForm.tsx
+++ src/app/components/Forms/Auth/NCAForm.tsx
@@ -9,7 +9,7 @@ export function NCAForm() {
     //JSX
     return (<div className={styles.conteiner}>
                 <div className="flex flex-col items-center justify-center">
-                    <Image src='assets/docker_logo.png' alt="Docker Lab" width={233} height={60} className="mb-9"/>
+                    <Image src='/assets/docker_logo.png' alt="Docker Lab" width={233} height={60} className="mb-9"/>
                     <div>Login com</div>
                     <GoogleBtn/>
                 </div>
