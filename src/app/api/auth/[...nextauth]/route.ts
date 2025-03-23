3431c0513c52f4b2f41698355b1c69a17db87b6e
Thiago Paiva Freire <thiago.freire@ifma.edu.br>
2025-03-23 16:57:54 -0300

    [FIX] Ajustes no NextAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth, { DefaultSession, Session, User } from "next-auth";
+import NextAuth from "next-auth";
 // import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { sendUsertoAPI } from "@/app/server/login/actions";
