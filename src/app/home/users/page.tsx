efd5be00d70e4190fa560ed86dca1e1060ace072
Thiago Paiva Freire <thiago.freire@ifma.edu.br>
2025-04-07 20:14:41 -0300

    [FIX] Ajustes no meno do perfil Admin

--- src/app/home/users/page.tsx
+++ src/app/home/users/page.tsx
@@ -17,8 +17,6 @@ export default function Page(){
 
         const verifySession = async () => {
             const session = await getSession();
-            if(session && session.user)
-                console.log(session.user);
             return session;
         }
     
@@ -26,9 +24,7 @@ export default function Page(){
             if(session){
                 const user = session.user;
     
-                console.log(user);
-    
-                if(user.profile != 'N')
+                if(user && user.profile != 'N')
                     setProfile(user.profile);
             }
         });
